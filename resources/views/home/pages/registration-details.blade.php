4eb8cbc3d3b531711dddbcfabdf62743acd97550
pcreyes9 <pcreyes09@gmail.com>
2024-07-18 16:39:08 +0800

    UPDATE REG DEETS

--- resources/views/home/pages/registration-details.blade.php
+++ resources/views/home/pages/registration-details.blade.php
@@ -11,7 +11,7 @@
         <div class="row justify-content-center mb-5" data-animation-in="slideInLeft" data-duration-in="1.2">
             <div class="col-lg-12 mb-5" >
                 <h3 class="into-sub-title" >The 24th ASEAN Congress of Anesthesiologists (ACA 2024)</h3>
-                <h3 class="section-header">23rd - 24th October 2025 | Manila Marriot Hotel</h3>
+                <h3 class="section-header">23rd - 25th October 2025 | Manila Marriott Hotel | Pasay | Philippines</h3>
             </div>
             <div class="col-lg-9 mt-4 mt-lg-0">
                 <p class="para-head">Registration Guidelines</p>
@@ -122,12 +122,11 @@
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas semper nunc nec ornare ornare. Nam sagittis elit ac ligula bibendum condimentum. In sodales, neque et tincidunt congue, lorem dui interdum arcu, id lobortis tortor nibh quis magna. Pellentesque sit amet semper turpis, non feugiat tellus. Duis malesuada nibh nec suscipit volutpat.
                 </p>
             </div>
-            <div class="d-grid gap-2 w-full mt-4">
-                <a href="{{route('memReg')}}" class="btn btn-primary" style="font-size: 20px">Local Registration</a>
-                <a href="#" class="btn btn-dark" style="font-size: 20px">International Registration</a>
-            </div>
         </div>
-        
+        <div class="d-grid gap-2 w-full mt-4">
+            <a href="{{route('memReg')}}" class="btn btn-primary" style="font-size: 20px">Local Registration</a>
+            <a href="#" class="btn btn-dark" style="font-size: 20px">International Registration</a>
+        </div>
     </div>
 </section><!-- Main container end -->
 @endsection

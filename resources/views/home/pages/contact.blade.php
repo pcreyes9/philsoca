2dd344c190b3e227fcd5279fda2062e7545b0ef7
pcreyes9 <pcreyes09@gmail.com>
2024-11-07 09:26:34 +0800

    contact pg upd

--- resources/views/home/pages/contact.blade.php
+++ resources/views/home/pages/contact.blade.php
@@ -16,17 +16,44 @@
             </div>
         </div>
 
-        <div class="d-flex justify-content-center px-5 mx-3">
-            <div class="ts-service-box-bg text-center h-100 px-5">
+        <div class="row">
+            {{-- <div class="col-md-4">
+              <div class="ts-service-box-bg text-center h-100">
                 <span class="ts-service-icon icon-round">
-                    <i class="fa fa-envelope mr-0"></i>
+                  <i class="fas fa-map-marker-alt mr-0"></i>
                 </span>
-                <div class="ts-service-box-content px-5">
-                    <h4>Email Us</h4>
-                    <p>ACAsecretariat2025@gmail.com</p>
+                <div class="ts-service-box-content">
+                  <h4>Visit Our Office</h4>
+                  <p>Room 102, PMA Building, 1100 North Ave, Diliman, Quezon City, 1100 Metro Manila, Philippines</p>
                 </div>
-            </div>
-        </div><!-- Col 2 end -->
+              </div>
+            </div><!-- Col 1 end --> --}}
+      
+            <div class="col-md-6">
+              <div class="ts-service-box-bg text-center h-100">
+                <span class="ts-service-icon icon-round">
+                  <i class="fa fa-envelope mr-0"></i>
+                </span>
+                <div class="ts-service-box-content">
+                  <h4>Email Us</h4>
+                  <p>ACAsecretariat2025@gmail.com</p>
+                </div>
+              </div>
+            </div><!-- Col 2 end -->
+      
+            <div class="col-md-6">
+              <div class="ts-service-box-bg text-center h-100">
+                <span class="ts-service-icon icon-round">
+                  <i class="fa fa-phone-square mr-0"></i>
+                </span>
+                <div class="ts-service-box-content">
+                  <h4>Call Us</h4>
+                  <p>Tel: 89295852 | 84522058</p>
+                </div>
+              </div>
+            </div><!-- Col 3 end -->
+      
+          </div><!-- 1st row end -->
 
         <div class="gap-60"></div>
         

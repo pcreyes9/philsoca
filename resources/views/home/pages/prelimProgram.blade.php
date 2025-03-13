d4b13e20f81b02be0ab6d9ac43186bcad92c9ece
Paul Reyes <pcreyes09@gmail.com>
2025-03-13 10:46:28 +0800

    udp to aca website

--- resources/views/home/pages/prelimProgram.blade.php
+++ resources/views/home/pages/prelimProgram.blade.php
@@ -16,6 +16,39 @@
             <div class="d-flex justify-content-center mt-5">
                 <img style="width: 100%;" class="align-middle mt-3 " src="images/prelim sci program day 3.jpg" alt="Image is still in process.">
             </div>
+            {{-- <h3 class="section-sub-title">Preliminary Scientific Program</h3>
+
+            <p class="para-head fd mt-5 mb-3">October 23, 2025: Day 1</p>
+                <table class="table table-striped-columns text-center table-bordered table-program table-responsive">
+                    <thead>
+                        <tr style="background: rgba(242, 222, 2, .7); ">
+                          <th class="align-middle" scope="col" width="8%">AM SESSION</th>
+                          <th class="align-middle" scope="col" width="23%">BALLROOM A</th>
+                          <th class="align-middle" scope="col" width="23%">BALLROOM B</th>
+                          <th class="align-middle" scope="col" width="23%">BALLROOM C </th>
+                          <th class="align-middle" scope="col" width="9%">Executive 7 & 9 <br> (Ground Floor)</th>
+                          <th class="align-middle" scope="col" width="8%">VIP EAST <br> (3rd Floor)</th>
+                          <th class="align-middle" scope="col" width="9%">Executive 5 <br> (Ground Floor)</th>
+                          <th class="align-middle" scope="col" width="9%">Executive 8 & 10 <br> (Ground Floor)</th>
+
+                        </tr>
+                      </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">8:30-9:00</th>
+                            <td class="text-left">SAY WAN LIM Memorial Lecture</td>
+                            <td class="text-left">WHO ECO Resolution, Health Emergencies and WFSA</td>
+                            <td class="text-left"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">8:30-9:00</th>
+                            <td class="text-left">SAY WAN LIM Memorial Lecture</td>
+                            <td class="text-left">WHO ECO Resolution, Health Emergencies and WFSA</td>
+                            <td class="text-left"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </p> --}}
         </div><!-- Container end -->
     </section><!-- Main container end -->
 @endsection

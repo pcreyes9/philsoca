346e5477607bd730bcd35ed2708e3ea17838c525
Paul Reyes <pcreyes09@gmail.com>
2025-03-14 11:20:01 +0800

    upd

--- resources/views/home/sponsors.blade.php
+++ resources/views/home/sponsors.blade.php
@@ -7,8 +7,15 @@
 @section('content')
 {{-- @include('home.banner') --}}
 
+
+
+
 <section id="news" class="project-area">
     <div class="container" >
+
+        <div>
+            <h3 class="py-2 text-center pb-5" style="font-weight: 700; font-size: 25px; text-transform: none">For Sponsorship, write an email to: <span style="font-style: italic; font-size: 25px;">sponsorshipaca2025@gmail.com</span></h3>
+        </div>
         {{-- style="background: rgba(255, 255, 255, 0.5); border-radius: 25px" --}}
         <div class="d-flex justify-content-center">
             <img style="width: 100%;" class="align-middle mt-3 " src="images/sponsors/gold.jpg" alt="Image is still in process.">

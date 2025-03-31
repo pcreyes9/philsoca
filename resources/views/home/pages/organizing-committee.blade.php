f57f4f72938e6d1721e0e793af23e589bc2e0535
pcreyes9 <pcreyes09@gmail.com>
2025-03-31 17:04:54 +0800

    upd

--- resources/views/home/pages/organizing-committee.blade.php
+++ resources/views/home/pages/organizing-committee.blade.php
@@ -6,9 +6,9 @@
 @section('content')
     {{-- @include('home.banner') --}}
     <section id="main-container" class="main-container text-center project-area ">
-        <div class="container" >
+        <div class="" >
             <div class="d-flex justify-content-center">
-                <img style="width: 100%;" class="align-middle "src="images/orgcom/ACA Scientific Committee Design.png?v={{ time() }}" alt="Image is still in process.">
+                <img style="width: 100%;" class="align-middle "src="images/orgcom/ACA Scientific Committee Design4.png?v={{ time() }}" alt="Image is still in process.">
             </div>    
         </div><!-- Container end -->
     </section><!-- Main container end -->

5d2e56fe6ebc2375d712a52fa50b952378301673
pcreyes9 <pcreyes09@gmail.com>
2025-04-02 16:22:22 +0800

    upd prizes

--- resources/views/home/pages/organizing-committee.blade.php
+++ resources/views/home/pages/organizing-committee.blade.php
@@ -5,11 +5,84 @@
 
 @section('content')
     {{-- @include('home.banner') --}}
-    <section id="main-container" class="main-container text-center project-area ">
-        <div class="" >
-            <div class="d-flex justify-content-center">
-                <img style="width: 100%;" class="align-middle "src="images/orgcom/ACA Scientific Committee Design4.png?v={{ time() }}" alt="Image is still in process.">
-            </div>    
-        </div><!-- Container end -->
+    <section id="main-container" class="main-container text-center project-area">
+        <div class="container" >
+            <div class="row justify-content-center">
+                <div class="col-lg-12" >
+                    {{-- <h3 class="into-sub-title .d-md-none .d-lg-block" style="color: #000066">The 24th ASEAN Congress of Anesthesiologists (ACA 2024) Organizing Committee</h3> --}}
+                    {{-- font-size: 2.1vmax;  --}}
+                    <h3 class="org-title" style="color: #000066;">The 24<sup style="text-transform: lowercase">th</sup> ASEAN Congress of Anesthesiologists (ACA 2025) <br> Organizing Committee</h3>
+                    <div class="row justify-content-center mt-5">
+                        <div class="col-auto">
+                            <table class="table borderless text-left table-responsive" >
+                                <tbody>
+                                    <tr>
+                                        <th scope="row"><strong>Overall Chair: </strong></th>
+                                        <td>Dr. Peñafrancia C. Cano</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Honorary Chair: </strong></th>
+                                        <td>Dr. Francis B. Mayuga</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Assistant Chair of Organizing Committee: </strong></th>
+                                        <td>Dr. Melissa M. Caranto</td>
+                                    </tr>
+                                    {{-- <tr>
+                                        <th scope="row"><strong>Scientific Committee Chair: </strong></th>
+                                        <td>Dr. Celina Z. Ancheta</td>
+                                    </tr> --}}
+                                    <tr>
+                                        <th scope="row"><strong>Scientific Committee: </strong> <br> &nbsp &nbsp Chair <br> &nbsp &nbsp Members</th>
+                                        <td><br>Dr. Celina Z. Ancheta<br>Dr. Peñafrancia C. Cano<br>Dr. Melissa M. Caranto</td>
+                                    </tr>
+                                    <tr >
+                                        <th scope="row" ><strong>Registration: </strong></th>
+                                        <td>Dr. Ma. Janetth B. Serrano</td>
+                                    </tr>
+                                    {{-- <tr>
+                                        <th scope="row"><strong>Finance: </strong></th>
+                                        <td>Dr. Carmelita S. Lunas </td>
+                                    </tr> --}}
+                                    <tr>
+                                        <th scope="row"><strong>Ways & Means: </strong></th>
+                                        <td>Dr. Edgard M. Simon <br> Dr. Zenaida M. Mariano</td>
+                                    </tr>
+                                    <tr >
+                                        <th scope="row" ><strong>Physical Arrangement: </strong></th>
+                                        <td>Dr. Salvador J. Brodit <br> Dr. Godfrey G. Agcon</td>
+                                        
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Trade & Exhibits: </strong></th>
+                                        <td>Dr. Francis B. Mayuga</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Research:</strong> <br> &nbsp &nbsp Chair <br> &nbsp &nbsp Members</th>
+                                        <td><br> Dr. Julie B. Balacuit <br> Dr. Melissa M. Caranto <br> Dr. Emmanuel S. Prudente <br> Dr. Aileen L. Rosales</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Social Events:</strong> <br> &nbsp &nbsp Chair <br> &nbsp &nbsp Members</th>
+                                        <td><br> Dr. Carmelita S. Lunas <br> Dr. Arnel M. Asino <br> Dr. Cherynne Inso-De Guzman</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Meet & Greet: </strong></th>
+                                        <td>Dr. Joselito T. Morete</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Souvenir Program: </strong></th>
+                                        <td>Dr. Jonnel L. Lim <br> Dr. Cristina Mayo Milo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"><strong>Advisory Board: </strong></th>
+                                        <td>Dr. Florian R. Nuevo <br> Dr. Eric V. Nagtalon</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div><!-- Col end -->
+            </div><!-- Row end -->
+          </div><!-- Container end -->
     </section><!-- Main container end -->
 @endsection

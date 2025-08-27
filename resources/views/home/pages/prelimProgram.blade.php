9b336084ed198a85bd26adc3d53830b509186c0d
pcreyes9 <pcreyes09@gmail.com>
2025-08-27 17:09:38 +0800

    upd program again

--- resources/views/home/pages/prelimProgram.blade.php
+++ resources/views/home/pages/prelimProgram.blade.php
@@ -7,13 +7,13 @@
     <section id="news" class="project-area">
         <div class="px-2 px-lg-5 mx-lg-5" >
             <div class="d-flex justify-content-center">
-                <img style="width: 100%;" class="align-middle" src="images/sci program/sci program 1.png?v={{ time() }}" alt="Image is still in process.">
+                <img style="width: 100%;" class="align-middle" src="images/sci program/sci1.png?v={{ time() }}" alt="Image is still in process.">
             </div>
             <div class="d-flex justify-content-center">
-                <img style="width: 100%;" class="align-middle mt-5 " src="images/sci program/sci program 2.png?v={{ time() }}" alt="Image is still in process.">
+                <img style="width: 100%;" class="align-middle mt-5 " src="images/sci program/sci2.png?v={{ time() }}" alt="Image is still in process.">
             </div>
             <div class="d-flex justify-content-center">
-                <img style="width: 100%;" class="align-middle mt-5 " src="images/sci program/sci program 3.png?v={{ time() }}" alt="Image is still in process.">
+                <img style="width: 100%;" class="align-middle mt-5 " src="images/sci program/sci3.png?v={{ time() }}" alt="Image is still in process.">
             </div> 
         </div>
     </section>

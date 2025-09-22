af8ec217045e3808a84f4d2bcdd6ddeb66ffe92a
pcreyes9 <pcreyes09@gmail.com>
2025-09-22 09:45:41 +0800

    closed registration

--- resources/views/registration/workshop-registration.blade.php
+++ resources/views/registration/workshop-registration.blade.php
@@ -8,10 +8,9 @@
 
 <section id="main-container" class="main-container text-center project-area ">
     <div class="container">
-        <div class="row justify-content-center" data-animation-in="slideInLeft" data-duration-in="1.2">
+        {{-- <div class="row justify-content-center" data-animation-in="slideInLeft" data-duration-in="1.2">
             <div class="col-lg-12" >
                 <h3 class="section-sub-title">WORKSHOP Registration</h3>
-                {{-- <h4 class="mt-n5 mb-5" style="color: black; font-style: italic; "><strong>(For foreign delegates PSA ID checker)</strong></h4> --}}
                 <h5 class="mt-n2 mb-2" style="color: black; font-style: italic; ">For foreign delegates' PSA ID checker</h5>
                 <p class="mt-n3" style="color: black; font-style: italic; ">Enter your last name, and your PSA ID no. will appear</p>
             </div>
@@ -20,8 +19,9 @@
                     {{ session('success') }}
                 </div>
             @endif
-        </div>
-        <livewire:workshop-reg/>
+        </div> --}}
+        <h3 class="section-sub-title">Registration is still closed</h3>
+        {{-- <livewire:workshop-reg/> --}}
     </div>
 </section><!-- Main container end -->
 @endsection

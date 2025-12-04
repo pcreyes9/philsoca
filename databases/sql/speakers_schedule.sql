a69185a028e37081809fe5b7c9b04c98c15a4bb7
pcreyes9 <pcreyes09@gmail.com>
2025-12-04 20:25:26 +0800

    asd

---
+++ databases/sql/speakers_schedule.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 22, 2025 at 05:31 AM
+-- Server version: 11.8.3-MariaDB-log
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `u179905176_acaWeb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `speakers_schedule`
+--
+
+CREATE TABLE `speakers_schedule` (
+  `id` int(2) DEFAULT NULL,
+  `speaker_id` int(3) DEFAULT NULL,
+  `username` varchar(27) DEFAULT NULL,
+  `topic` varchar(97) DEFAULT NULL,
+  `venue` varchar(12) DEFAULT NULL,
+  `date` varchar(9) DEFAULT NULL,
+  `time` varchar(17) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Dumping data for table `speakers_schedule`
+--
+
+INSERT INTO `speakers_schedule` (`id`, `speaker_id`, `username`, `topic`, `venue`, `date`, `time`) VALUES
+(1, 50, 'Xavier Falieres', 'Perioperative Process: The Focused Factory Principle ', 'BALLROOM A', '23-Oct-25', '1:00 PM-1:20 PM'),
+(2, 93, 'Samuel Ern Hung Tsan', 'Neuroanesthesia Updates in Regional Anesthesia and Analgesia in Neurosurgery', 'BALLROOM C', '23-Oct-25', '1:00 PM-1:20 PM'),
+(3, 134, 'Sng Ban Leong', 'Obstetric Update on Non-neuraxial Labour Analgesia ', 'BALLROOM B', '23-Oct-25', '1:00 PM-1:20 PM'),
+(4, 129, 'Varinee Lekprasert', 'The Impact of Perioperative Care on Patient Outcomes ', 'BALLROOM A', '23-Oct-25', '1:20 PM-1:40 PM'),
+(5, 130, 'Sanjib Adhikary', 'Myths and facts of Epidural Analgesia ', 'BALLROOM B', '23-Oct-25', '1:20 PM-1:40 PM'),
+(6, 179, 'Jeffrey Pasternak', 'The Pregnant Patient and Neurosurgery', 'BALLROOM C', '23-Oct-25', '1:20 PM-1:40 PM'),
+(7, 125, 'Balavenkat Subramanian', 'Anesthesia for Scoliosis Surgery - An Update', 'BALLROOM C', '23-Oct-25', '1:40 PM-2:00 PM'),
+(8, 126, 'Alex Sia Tiong Heng', 'Post Dural Puncture Headache: The Not-So-Good, The Bad and the Ugly', 'BALLROOM B', '23-Oct-25', '1:40 PM-2:00 PM'),
+(9, 132, 'Patrick Giam', 'Advancing Patient Safety', 'BALLROOM A', '23-Oct-25', '1:40 PM-2:00 PM'),
+(10, 58, 'Sophia Chew', 'Geroprotection and Anesthesiology', 'BALLROOM C', '23-Oct-25', '2:00 PM-2:20 PM'),
+(11, 97, 'Heather Nixon', 'Intraoperative Pain During Cesarian Delivery-Optimizing Team Care', 'BALLROOM B', '23-Oct-25', '2:00 PM-2:20 PM'),
+(12, 151, 'Rodney Gabriel', 'Leveraging Artificial Intelligence in Perioperative Medicine', 'BALLROOM A', '23-Oct-25', '2:00 PM-2:20 PM'),
+(13, 137, 'Shahridan Mohd Fathil', 'Monitoring Standards for Anesthesia', 'BALLROOM C', '23-Oct-25', '3:20 PM-3:40 PM'),
+(14, 138, 'Athmaja Thottungal', 'Gut Microbiome and Pain- Practical Pearls', 'BALLROOM B', '23-Oct-25', '3:20 PM-3:40 PM'),
+(15, 175, 'Suneel Ramesh Desai', 'Hemodynamic Assessment With Neck Vessels', 'BALLROOM A', '23-Oct-25', '3:20 PM-3:40 PM'),
+(16, 49, 'Suraphong Lorsomradee', 'ECG-SpO2 Derived Cardiac Output for Simple High Risk Surgery', 'BALLROOM A', '23-Oct-25', '3:40 PM-4:00 PM'),
+(17, 52, 'Christopher Bowden', 'Ketamine - updates in our clinical practice', 'BALLROOM B', '23-Oct-25', '3:40 PM-4:00 PM'),
+(18, 127, 'Azarinah Izaham', 'New Horizons in Perioperative Risk Assesment Tools: From AI to Clinical Practice...Neuromuscular', 'BALLROOM C', '23-Oct-25', '3:40 PM-4:00 PM'),
+(19, 60, 'Bishwas Pradhan', 'Goal Directed Fluid therapy in Critical Care', 'BALLROOM C', '23-Oct-25', '4:00 PM-4:20 PM'),
+(20, 131, 'Yong Chow Yen', 'Permissive Hypotension: How Low Can You Go Dato', 'BALLROOM A', '23-Oct-25', '4:00 PM-4:20 PM'),
+(21, 164, 'Carina Li', 'Visceral Abdominal Cancer Pain Management with Neurolytic Blocks', 'BALLROOM B', '23-Oct-25', '4:00 PM-4:20 PM'),
+(22, 125, 'Balavenkat Subramanian', 'Anesthesia for Orthogeriatrics', 'BALLROOM B', '23-Oct-25', '4:20 PM-4:40 PM'),
+(23, 139, 'Phuping Akavipat', 'Anesthesia Considerations in Neurological Emergencies', 'BALLROOM C', '23-Oct-25', '4:20 PM-4:40 PM'),
+(24, 144, 'Suresh Paranjothy', 'How not to fail with a patient with Heart Failure', 'BALLROOM A', '23-Oct-25', '4:20 PM-4:40 PM'),
+(25, 133, 'Daniela Filipescu', 'SAY WAN LIM Memorial Lecture: WHO ECO Resolution, Health Emergencies and WFSA ', 'PLENARY HALL', '23-Oct-25', '8:30 AM-9:00 AM'),
+(26, 141, 'Susilo Chandra', 'QUINTIN GOMEZ MEMORIAL LECTURE: Opioid Free Anesthesia: The Next Frontier in Anesthesia Practice ', 'PLENARY HALL', '23-Oct-25', '9:00 AM-9:30 AM'),
+(27, 173, 'Afak Nsiri', 'Implementing Patient Blood Management (PBM) in an Orthopedic Department', 'BALLROOM A', '24-Oct-25', '1:10 AM-1:30 PM'),
+(28, 97, 'Heather Nixon', 'Medication Safety in Obstetric Anesthesia', 'BALLROOM C', '24-Oct-25', '1:10 PM-1:30 PM'),
+(29, 131, 'Yong Chow Yen', 'Understanding Systemic Inflammatory Response During Cardiac Surgery DATO', 'BALLROOM B', '24-Oct-25', '1:10 PM-1:30 PM'),
+(30, 48, 'Hasmizy Muhammad', 'Pulmonary Embolism: Recognition and Management', 'BALLROOM B', '24-Oct-25', '10:30 AM-10:50 AM'),
+(31, 175, 'Suneel Ramesh Desai', 'Preoperative Cardiac Risk Assessment for NonCardiac Surgery', 'BALLROOM A', '24-Oct-25', '10:30 AM-10:50 AM'),
+(32, 178, 'Nabil Elkassabany', 'Analgesic Strategies for Knee Surgery', 'BALLROOM C', '24-Oct-25', '10:30 AM-10:50 AM'),
+(33, 58, 'Sophia Chew', 'Improving Outcomes in High Risk Patients', 'BALLROOM A', '24-Oct-25', '10:50 AM-11:10 AM'),
+(34, 93, 'Samuel Ern Hung Tsan', 'Targeted Temperature Management Post Cardiac Arrest', 'BALLROOM B', '24-Oct-25', '10:50 AM-11:10 AM'),
+(35, 173, 'Afak Nsiri', 'Hip Fracture: Which Anesthetic Techniques Should We Choose', 'BALLROOM C', '24-Oct-25', '10:50 AM-11:10 AM'),
+(36, 169, 'ASHOKKA BALAKRISHNAN', 'POCUS in Cardiac Arrest', 'BALLROOM A', '24-Oct-25', '11:10 AM-11:30 AM'),
+(37, 179, 'Jeffrey Pasternak', 'Anesthetic Management in Patients with Ischemic Stroke', 'BALLROOM B', '24-Oct-25', '11:10 AM-11:30 AM'),
+(38, 180, 'Jean-Louis Horn', 'Continuous Nerve Blocks: Why, When and How to Manage', 'BALLROOM C', '24-Oct-25', '11:10 AM-11:30 AM'),
+(39, 49, 'Suraphong Lorsomradee', 'AMBU/BLOOD MANAGEMENT of Ambulatory in the ASEAN Region', 'BALLROOM A', '24-Oct-25', '12:30 PM-12:50 PM'),
+(40, 134, 'Sng Ban Leong', 'Education in obstetric anesthesia-what’s new', 'BALLROOM C', '24-Oct-25', '12:30 PM-12:50 PM'),
+(41, 172, 'Wanda Maria Popescu', 'THORACIC/CARDIAC: The Future of Thoracic Anesthesia', 'BALLROOM B', '24-Oct-25', '12:30 PM-12:50 PM'),
+(42, 60, 'Bishwas Pradhan', 'Lung Recruitment Maneuvers – Efficacy & Safety', 'BALLROOM B', '24-Oct-25', '12:50 PM-1:10 PM'),
+(43, 126, 'Alex Sia Tiong Heng', 'AI in Obstetric Anesthesia and Analgesia', 'BALLROOM C', '24-Oct-25', '12:50 PM-1:10 PM'),
+(44, 133, 'Daniela Filipescu', 'Old Habits Die Hard in Perioperative Haemostasis and Transfusion', 'BALLROOM A', '24-Oct-25', '12:50 PM-1:10 PM'),
+(45, 141, 'Susilo Chandra', 'Opioid Free Anesthesia for OBSTETRIC and GYNECOLOGIC Surgery', 'BALLROOM C', '25-Oct-25', '8:00 AM-8:20 AM'),
+(46, 47, 'Tony Ng', 'Update in Regenerative Medicine in Pain Management', 'BALLROOM B', '25-Oct-25', '1:00 PM-1:20 PM'),
+(47, 127, 'Azarinah Izaham', 'ERAS in Ambulatory Anesthesia: Beyond Traditional Protocols', 'BALLROOM A', '25-Oct-25', '1:00 PM-1:20 PM'),
+(48, 174, 'Raveenthiran Rasiah', 'Guidelines for TIVA/TCI in Pediatrics', 'BALLROOM C', '25-Oct-25', '1:00 PM-1:20 PM'),
+(49, 173, 'Afak Nsiri', 'Can Regional Anesthesia Facilitate Enhanced Recovery After Surgery (ERAS)?', 'BALLROOM A', '25-Oct-25', '1:20 AM-1:40 PM'),
+(50, 152, 'Vereecke Hugo', 'Proper Disposal of Propofol to Mitigate Its Ecological Impact', 'BALLROOM C', '25-Oct-25', '1:20 PM-1:40 PM'),
+(51, 161, 'KHALIL ULLAH SHIBLI', 'Pain Mechanisms in the Elderly', 'BALLROOM B', '25-Oct-25', '1:20 PM-1:40 PM'),
+(52, 181, 'Andriamuri Primaputra Lubis', 'Optimizing the COPD Patient for Ambulatory Surgery', 'BALLROOM A', '25-Oct-25', '1:40 AM-2:00 PM'),
+(53, 129, 'Varinee Lekprasert', 'Understanding Processed EEG During Anesthesia', 'BALLROOM C', '25-Oct-25', '1:40 PM-2:00 PM'),
+(54, 154, 'Shigeki Yamaguchi', 'Opioid-Induced Depression and It\'s Monitoring', 'BALLROOM B', '25-Oct-25', '1:40 PM-2:00 PM'),
+(55, 53, 'Vesna Jevtovic-Todorovic', 'Anesthetics as Neurotoxins', 'BALLROOM B', '25-Oct-25', '10:10 AM-10:30 AM'),
+(56, 108, 'Annery Garcia-Marcinkiewicz', 'Evidenced Based Approach to Neonatal Airway', 'BALLROOM A', '25-Oct-25', '10:10 AM-10:30 AM'),
+(57, 180, 'Jean-Louis Horn', 'Local Anesthetic Resistance: Myth or Reality', 'BALLROOM C', '25-Oct-25', '10:10 AM-10:30 AM'),
+(58, 115, 'Ina Ismiarty Shariffuddin', 'Patient Blood Management in Pediatric Surgery', 'BALLROOM B', '25-Oct-25', '10:30 AM-10:50 AM'),
+(59, 130, 'Sanjib Adhikary', 'Does Fascial Plane Blocks Work?', 'BALLROOM C', '25-Oct-25', '10:30 AM-10:50 AM'),
+(60, 168, 'Yew Weng Chan', 'Updates on Supraglottic Airway Devices', 'BALLROOM A', '25-Oct-25', '10:30 AM-10:50 AM'),
+(61, 46, 'JINTAE Kim', 'Clinically Useful Regional Blocks for Pediatric Patients', 'BALLROOM B', '25-Oct-25', '10:50 AM-11:10 AM'),
+(62, 137, 'Shahridan Mohd Fathil', 'Airway Ultrasound in Cannot Intubate and Cannot Ventilate Scenario', 'BALLROOM A', '25-Oct-25', '10:50 AM-11:10 AM'),
+(63, 164, 'Carina Li', 'Neuraxial Anesthesia after Spine Surgery', 'BALLROOM C', '25-Oct-25', '10:50 AM-11:10 AM'),
+(64, 53, 'Vesna Jevtovic-Todorovic', 'Effect of General Anesthesia on the Neonate Brain', 'BALLROOM B', '25-Oct-25', '11:10 AM-11:30 AM'),
+(65, 172, 'Wanda Maria Popescu', 'A lower FiO2 regimen during Thoracic Surgery', 'BALLROOM A', '25-Oct-25', '11:10 AM-11:30 AM'),
+(66, 178, 'Nabil Elkassabany', 'What Outcomes Metrics Should Be Measured in Regional Anesthesia and Acute Pain Medicine', 'BALLROOM C', '25-Oct-25', '11:10 AM-11:30 AM'),
+(67, 50, 'Xavier Falieres', 'Ambulatory Surgery, Pushing the Limits', 'BALLROOM A', '25-Oct-25', '12:40 PM-1:00 PM'),
+(68, 151, 'Rodney Gabriel', 'Applications of Cryoneurolysis for Pain Management', 'BALLROOM B', '25-Oct-25', '12:40 PM-1:00 PM'),
+(69, 152, 'Vereecke Hugo', 'Understanding TCI', 'BALLROOM C', '25-Oct-25', '12:40 PM-1:00 PM'),
+(70, 47, 'Tony Ng', 'Advances in Non-invasive Pain Intervention Options', 'BALLROOM B', '25-Oct-25', '8:00 AM-8:20 AM'),
+(71, 58, 'Sophia Chew', 'A Brave New World - Challenges in Anesthesiology', 'BALLROOM A', '25-Oct-25', '8:00 AM-8:20 AM'),
+(72, 132, 'Patrick Giam', 'Leading in Anesthesiology: From Operating Room to the ASA Presidency', 'BALLROOM A', '25-Oct-25', '8:20 AM-8:40 AM'),
+(73, 138, 'Athmaja Thottungal', 'Men are from Mars, Women are from Venus. Gender Difference in Pain and Pain Management', 'BALLROOM B', '25-Oct-25', '8:20 AM-8:40 AM'),
+(74, 170, 'Ma Concepcion L Cruz', 'Uterotonics', 'BALLROOM C', '25-Oct-25', '8:20 AM-8:40 AM'),
+(75, 52, 'Christopher Bowden', 'Obstetric Hemorrhage - why mothers still die', 'BALLROOM C', '25-Oct-25', '8:40 AM-9:00 AM'),
+(76, 154, 'Shigeki Yamaguchi', 'Managing Chronic Pain in Cancer Survivors', 'BALLROOM B', '25-Oct-25', '8:40 AM-9:00 AM'),
+(77, 171, 'Klaus Goerlinger', 'New Paradigm in Coagulation Management', 'BALLROOM A', '25-Oct-25', '8:40 AM-9:00 AM'),
+(78, 97, 'Heather Nixon', 'Simulation in Obstetric Anesthesia - How to Get Your Team to Say and Do the Right Things', 'BALLROOM C', '25-Oct-25', '9:00 AM-9:20 AM'),
+(79, 135, 'Sasikaan Nimmaanrat', 'Developing Quality Assurance for Pain Management', 'BALLROOM B', '25-Oct-25', '9:00 AM-9:20 AM'),
+(80, 161, 'KHALIL ULLAH SHIBLI', 'Perioperative Care of the Obese Patient', 'BALLROOM A', '25-Oct-25', '9:00 AM-9:20 AM'),
+(81, 140, 'Maria Lourdes Mendoza', '', '', '', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

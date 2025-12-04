85373cf4f2ecfcca294975c25c49504539cb0bb2
pcreyes9 <pcreyes09@gmail.com>
2025-12-04 16:53:00 +0800

    spinner added

--- resources/views/home/gallery/display.blade.php
+++ resources/views/home/gallery/display.blade.php
@@ -4,118 +4,130 @@
 @section('banner-name', 'PSA: Midyar Convention')
 
 @section('content')
-    {{-- @include('home.banner') --}}
 
-    <section id="project-area" class="project-area solid-bg">
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-lg-12">
-                    <h2 class="section-title">ASEAN CONGRESS OF ANESTHESIOLOGISTS 2025 </h2>
-                    <h3 class="section-sub-title">DAY 1</h3>
-                </div>
-            </div>
-            <!--/ Title row end -->
+<div id="page-loader">
+    <div class="loader-content">
+        <div class="spinner"></div>
+        <p class="loader-text">Images are loading...</p>
+    </div>
+</div>
 
-            <div class="row">
-                <div class="col-12">
-                    <div class="shuffle-btn-group">
-                        <label class="active" for="all">
-                            <input type="radio" name="shuffle-filter" id="all" value="all" checked="checked">Show All
-                        </label>
-                        <label for="opening">
-                            <input type="radio" name="shuffle-filter" id="opening" value="opening">Opening Ceremony
-                        </label>
-                        <label for="registration">
-                            <input type="radio" name="shuffle-filter" id="registration" value="registration">Registration
-                        </label>
-                        <label for="asean">
-                            <input type="radio" name="shuffle-filter" id="asean" value="asean">ASEAN Night
-                        </label>
-                        {{-- <label for="government">
-                            <input type="radio" name="shuffle-filter" id="government" value="government">Government
-                        </label>
-                        <label for="infrastructure">
-                            <input type="radio" name="shuffle-filter" id="infrastructure" value="infrastructure">Infrastructure
-                        </label>
-                        <label for="residential">
-                            <input type="radio" name="shuffle-filter" id="residential" value="residential">Residential
-                        </label>
-                        <label for="healthcare">
-                            <input type="radio" name="shuffle-filter" id="healthcare" value="healthcare">Healthcare
-                        </label> --}}
-                    </div><!-- project filter end -->
+<section id="project-area" class="project-area solid-bg">
+<div class="container">
+    <div class="row text-center">
+        <div class="col-lg-12">
+            <h2 class="section-title">ASEAN CONGRESS OF ANESTHESIOLOGISTS 2025 </h2>
+            <h3 class="section-sub-title">DAY 1</h3>
+        </div>
+    </div>
+    <!--/ Title row end -->
+
+    <div class="row">
+        <div class="col-12">
+            <div class="shuffle-btn-group">
+                <label class="active" for="all">
+                    <input type="radio" name="shuffle-filter" id="all" value="all" checked="checked">Show All
+                </label>
+                <label for="opening">
+                    <input type="radio" name="shuffle-filter" id="opening" value="opening">Opening Ceremony
+                </label>
+                <label for="registration">
+                    <input type="radio" name="shuffle-filter" id="registration" value="registration">Registration
+                </label>
+                <label for="asean">
+                    <input type="radio" name="shuffle-filter" id="asean" value="asean">ASEAN Night
+                </label>
+                {{-- <label for="government">
+                    <input type="radio" name="shuffle-filter" id="government" value="government">Government
+                </label>
+                <label for="infrastructure">
+                    <input type="radio" name="shuffle-filter" id="infrastructure" value="infrastructure">Infrastructure
+                </label>
+                <label for="residential">
+                    <input type="radio" name="shuffle-filter" id="residential" value="residential">Residential
+                </label>
+                <label for="healthcare">
+                    <input type="radio" name="shuffle-filter" id="healthcare" value="healthcare">Healthcare
+                </label> --}}
+            </div><!-- project filter end -->
 
 
-                    <div class="row shuffle-wrapper">
-                        <div class="col-1 shuffle-sizer"></div>
-                        @foreach ($arrOpening as $pic)
-                            <div class="col-lg-4 col-md-6 col-6 shuffle-item p-1" data-groups="[&quot;opening&quot;]">
-                                <div class="project-img-container">
-                                    <a class="gallery-popup" href="images/gallery/day 1/opening ceremony/{{ $pic }}.JPG" aria-label="project-img">
-                                        <img class="img-fluid" src="images/gallery/day 1/opening ceremony/{{ $pic }}.JPG" alt="project-img">
-                                        <span class="gallery-icon"><i class="fa fa-plus"></i></span>
-                                    </a>
-                                    {{-- <div class="project-item-info">
-                                        <div class="project-item-info-content">
-                                            <h3 class="project-item-title">
-                                                <a href="projects-single.html">Capital Teltway Building</a>
-                                            </h3>
-                                            <p class="project-cat">opening, Interiors</p>
-                                        </div>
-                                    </div> --}}
+            <div class="row shuffle-wrapper">
+                <div class="col-1 shuffle-sizer"></div>
+                @foreach ($arrOpening as $pic)
+                    <div class="col-lg-4 col-md-6 col-6 shuffle-item p-1" data-groups="[&quot;opening&quot;]">
+                        <div class="project-img-container">
+                            <a class="gallery-popup" href="images/gallery/day 1/opening ceremony/{{ $pic }}.JPG" aria-label="project-img">
+                                <img class="img-fluid" src="images/gallery/day 1/opening ceremony/{{ $pic }}.JPG" alt="project-img">
+                                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+                            </a>
+                            {{-- <div class="project-item-info">
+                                <div class="project-item-info-content">
+                                    <h3 class="project-item-title">
+                                        <a href="projects-single.html">Capital Teltway Building</a>
+                                    </h3>
+                                    <p class="project-cat">opening, Interiors</p>
                                 </div>
-                            </div><!-- shuffle item 1 end -->
-                        @endforeach
-                        
-                        
-                        @foreach ($arrReg as $pic)
-                            <div class="col-lg-4 col-md-6 col-6 shuffle-item p-1" data-groups="[&quot;registration&quot;]">
-                                <div class="project-img-container">
-                                    <a class="gallery-popup" href="images/gallery/day 1/registration/{{ $pic }}.JPG" aria-label="project-img">
-                                        <img class="img-fluid" src="images/gallery/day 1/registration/{{ $pic }}.JPG" alt="project-img">
-                                        <span class="gallery-icon"><i class="fa fa-plus"></i></span>
-                                    </a>
-                                    {{-- <div class="project-item-info">
-                                        <div class="project-item-info-content">
-                                            <h3 class="project-item-title">
-                                                <a href="projects-single.html">Capital Teltway Building</a>
-                                            </h3>
-                                            <p class="project-cat">opening, Interiors</p>
-                                        </div>
-                                    </div> --}}
+                            </div> --}}
+                        </div>
+                    </div><!-- shuffle item 1 end -->
+                @endforeach
+                
+                
+                @foreach ($arrReg as $pic)
+                    <div class="col-lg-4 col-md-6 col-6 shuffle-item p-1" data-groups="[&quot;registration&quot;]">
+                        <div class="project-img-container">
+                            <a class="gallery-popup" href="images/gallery/day 1/registration/{{ $pic }}.JPG" aria-label="project-img">
+                                <img class="img-fluid" src="images/gallery/day 1/registration/{{ $pic }}.JPG" alt="project-img">
+                                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+                            </a>
+                            {{-- <div class="project-item-info">
+                                <div class="project-item-info-content">
+                                    <h3 class="project-item-title">
+                                        <a href="projects-single.html">Capital Teltway Building</a>
+                                    </h3>
+                                    <p class="project-cat">opening, Interiors</p>
                                 </div>
-                            </div><!-- shuffle item 1 end -->
-                        @endforeach
+                            </div> --}}
+                        </div>
+                    </div><!-- shuffle item 1 end -->
+                @endforeach
 
-                        @foreach ($arrAsean as $pic)
-                            <div class="col-lg-4 col-md-6 col-6 shuffle-item p-1" data-groups="[&quot;asean&quot;]">
-                                <div class="project-img-container">
-                                    <a class="gallery-popup" href="images/gallery/day 1/asean night/{{ $pic }}.JPG" aria-label="project-img">
-                                        <img class="img-fluid" src="images/gallery/day 1/asean night/{{ $pic }}.JPG" alt="project-img">
-                                        <span class="gallery-icon"><i class="fa fa-plus"></i></span>
-                                    </a>
-                                    {{-- <div class="project-item-info">
-                                        <div class="project-item-info-content">
-                                            <h3 class="project-item-title">
-                                                <a href="projects-single.html">Capital Teltway Building</a>
-                                            </h3>
-                                            <p class="project-cat">opening, Interiors</p>
-                                        </div>
-                                    </div> --}}
+                @foreach ($arrAsean as $pic)
+                    <div class="col-lg-4 col-md-6 col-6 shuffle-item p-1" data-groups="[&quot;asean&quot;]">
+                        <div class="project-img-container">
+                            <a class="gallery-popup" href="images/gallery/day 1/asean night/{{ $pic }}.JPG" aria-label="project-img">
+                                <img class="img-fluid" src="images/gallery/day 1/asean night/{{ $pic }}.JPG" alt="project-img">
+                                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+                            </a>
+                            {{-- <div class="project-item-info">
+                                <div class="project-item-info-content">
+                                    <h3 class="project-item-title">
+                                        <a href="projects-single.html">Capital Teltway Building</a>
+                                    </h3>
+                                    <p class="project-cat">opening, Interiors</p>
                                 </div>
-                            </div><!-- shuffle item 1 end -->
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="general-btn text-center">
-                    <a class="btn btn-primary" href="#">View All Photos</a>
-                </div>
+                            </div> --}}
+                        </div>
+                    </div><!-- shuffle item 1 end -->
+                @endforeach
             </div>
         </div>
-        <!--/ Container end -->
-        </section><!-- Project area end -->
-    
+    </div>
+    <div class="col-12">
+        <div class="general-btn text-center">
+            <a class="btn btn-primary" href="#">View All Photos</a>
+        </div>
+    </div>
+</div>
+<!--/ Container end -->
+</section><!-- Project area end -->
 @endsection
+<script>
+    window.addEventListener("load", function () {
+        const loader = document.getElementById("page-loader");
+        loader.style.opacity = "0";
+        setTimeout(() => loader.style.display = "none", 500);
+    });
+</script>
 
